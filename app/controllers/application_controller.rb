eb588dc8b5843d677995bf67c02e138b2394660c
yousuke44 <sf44nkss@icloud.com>
2021-06-15 11:38:36 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == 'nb120403'
+    end
+  end
 end
