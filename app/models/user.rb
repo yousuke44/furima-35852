53d98678adba1c564465ea77e7b81bf3ab7fdd79
yousuke44 <sf44nkss@icloud.com>
2021-06-18 15:49:56 +0900

    テストコード、バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname, presence: true
+    validates :date_of_birth, presence: true
+
+    with_options format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'Please enter your name in full-width' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'Please enter your name in full-width' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
   PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'contain both letters and nembers'
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'Please enter your name in full-width' } do
-    validates :last_name
-    validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'Please enter your name in full-width' } do
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence: true
-  end
-
-  validates :date_of_birth, presence: true
 end
